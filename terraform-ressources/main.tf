0df3cf968ddcc231b03ec6b223529c1492ac09ab
NOUMSI ULRICH STEVE <38357057+ulrich-sun@users.noreply.github.com>
2024-09-27 11:55:51 +0200

    add sg id

--- terraform-ressources/main.tf
+++ terraform-ressources/main.tf
@@ -10,21 +10,25 @@ data "aws_subnet" "ds-subnet" {
   id = "subnet-05091b46f3d662b8d"
 }
 
+data "aws_security_group" "ds-sg" {
+  id = "sg-0e1dd1f1ca497e251"
+}
+
 #module "vpc" {
 #  source = "./modules/vpc"
 #}
 
-module "sg" {
-  source = "./modules/sg"
-  sg_name = "ds-sg"
-  vpc_id = data.aws_vpc.ds-vpc.id
-}
+#module "sg" {
+#  source = "./modules/sg"
+#  sg_name = "ds-sg"
+#  vpc_id = data.aws_vpc.ds-vpc.id
+#}
 
 module "ec2" {
   source = "./modules/ec2"
   ami = "ami-0e86e20dae9224db8"
   instance_type = "t2.medium"
-  sg_id = module.sg.sg_id
+  sg_id = data.aws_security_group.id
   key_name = "sun"
   vm_name = var.vm_name
   subnet_id = data.aws_subnet.ds-subnet.id
