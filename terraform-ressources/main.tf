a7a965b333e88606b6b1cf9c192419990cf9752a
NOUMSI ULRICH STEVE <38357057+ulrich-sun@users.noreply.github.com>
2024-09-27 11:57:34 +0200

    update sg

--- terraform-ressources/main.tf
+++ terraform-ressources/main.tf
@@ -28,7 +28,7 @@ module "ec2" {
   source = "./modules/ec2"
   ami = "ami-0e86e20dae9224db8"
   instance_type = "t2.medium"
-  sg_id = data.aws_security_group.id
+  sg_id = data.aws_security_group.ds-sg.id
   key_name = "sun"
   vm_name = var.vm_name
   subnet_id = data.aws_subnet.ds-subnet.id
