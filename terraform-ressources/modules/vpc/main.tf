c84e1da93efa6d4ddef5853a8af2ed36944a89b8
NOUMSI ULRICH STEVE <38357057+ulrich-sun@users.noreply.github.com>
2024-09-27 11:00:37 +0200

    update

--- terraform-ressources/modules/vpc/main.tf
+++ terraform-ressources/modules/vpc/main.tf
@@ -27,7 +27,7 @@ resource "aws_internet_gateway" "igw" {
 
 resource "aws_route_table" "rt" {
   vpc_id = aws_vpc.vpc.id 
-  route = { 
+  route { 
     cidr_block = "0.0.0.0/0"
     gateway_id = aws_internet_gateway.igw.id
   }
