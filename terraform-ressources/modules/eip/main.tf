3a504ba6e7a6cdcb0fd5cd10aea5d313264c576e
Ubuntu <ubuntu@ip-172-31-84-194.ec2.internal>
2024-09-30 21:26:25 +0000

    upd

--- terraform-ressources/modules/eip/main.tf
+++ terraform-ressources/modules/eip/main.tf
@@ -2,10 +2,12 @@ resource "aws_eip" "eip" {
     domain = "vpc"
     tags = {
         Name = "eip"
-    }
-    provisioner "local-exec" {
-        command = <<-EOT
-            echo ${self.public_ip} > instance_ip.txt
-        EOT
-  }
+}
+provisioner "local-exec" {
+    command = "echo ${self.public_ip} > instance_ip.txt"
+}
+
+
+
+
 } 
